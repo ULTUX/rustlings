3da2ac4bf6c5699c4563cb3bf8b063a1667adf66
WNowak <git@wnowak.me>
2023-02-03 01:18:14 +0100

    progress

--- Understanding Ownership/What is ownership/Build String from Literals/src/main.rs
+++ Understanding Ownership/What is ownership/Build String from Literals/src/main.rs
@@ -2,6 +2,6 @@ fn main() {
     let part1 = "Rust ";
     let part2 = "is the ";
     let part3 = "greatest programming language";
-    // Write your code here
+    let message: String = String::from(part1) + part2 + part3;
     println!("{}", message)
 }
